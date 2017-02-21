e0838d770248f33a983ebdec3e1322ba0fd8c75a
Thanasis Papaioannou <thanasis.papaioannou@gmail.com>
2017-02-21 17:40:25 +0200

    SQLite connection added

--- clients/CloudServiceClient.java
+++ clients/CloudServiceClient.java
@@ -3,7 +3,42 @@ import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
 import java.util.Map;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CloudServiceClient {
+	
+	public Connection connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:serviceStats.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } 
+        return conn;
+    }
+	
+	public void insert(String name, double response_time) {
+        String sql = "INSERT INTO SERVICE_STATS(id,response_time) VALUES(?,?)";
+ 
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, response_time);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
+	
 
 	public static void abBenchmark(int requests, int concurrency, int buffer, String url)
 	{
